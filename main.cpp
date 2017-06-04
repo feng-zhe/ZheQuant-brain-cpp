c92be0edc091bfa7cb1cb9efdc620b27d5f652d2
fengzhe <henryzhefeng@gmail.com>
2017-06-04 13:59:01 +0800

    fix dump issue caused by message string

--- main.cpp
+++ main.cpp
@@ -71,10 +71,11 @@ int main()
         // start consuming from the queue, and install the callbacks
         ch_recv.consume(QUEUE_TODO, AMQP::durable)
             .onReceived( [&ch_recv, &ch_send, &EXCHG_DONE, &ROUTING_KEY_DONE](const AMQP::Message& message, uint64_t deliveryTag, bool redelivered) {
-                    cout << "[main] message received: " << message.body() << endl;
+                    string msg_str = string(message.body(), static_cast<size_t>(message.bodySize()));
+                    cout << "[main] message received: " << msg_str << endl;
                     // read message into json object
                     Document doc_msg, doc_result;
-                    doc_msg.Parse(message.body());
+                    doc_msg.Parse(msg_str.c_str());
                     doc_result.SetObject();
                     // check message
                     if( !doc_msg.HasMember("name") || !doc_msg["name"].IsString() ||
